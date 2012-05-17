2db3c6af63a78f7ca2c432e010808111ec295cf9
juliohenriquerocha <juliohenriquerocha@a881ba28-6254-5c3f-0099-06f9c60959e8>
2012-05-17 22:31:52 +0000

    Menu

--- Busao/src/br/edu/ufcg/dsc/busao/MenuActivity.java
+++ Busao/src/br/edu/ufcg/dsc/busao/MenuActivity.java
@@ -1,8 +1,11 @@
 package br.edu.ufcg.dsc.busao;
 
+import it.sephiroth.demo.slider.widget.MultiDirectionSlidingDrawer;
+
 import java.util.ArrayList;
 
 import android.app.Activity;
+import android.graphics.Typeface;
 import android.os.Bundle;
 import android.view.ViewGroup.LayoutParams;
 import android.widget.HorizontalScrollView;
@@ -16,7 +19,25 @@ public class MenuActivity extends Activity {
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.menu_principal);
+		setTextFont();
+		MultiDirectionSlidingDrawer drawer = (MultiDirectionSlidingDrawer) findViewById(R.id.drawer);
+		drawer.open();
 		
+		
+		
+	}
+	
+	private void setTextFont(){
+		//set Font
+		TextView textLocalidade = (TextView) findViewById(R.id.textLocalidade);  
+		TextView textBuscar = (TextView) findViewById(R.id.textBuscar);  
+		TextView textTurismo = (TextView) findViewById(R.id.textTurismo);  
+		TextView textAjuda = (TextView) findViewById(R.id.textAjuda);  
+		Typeface font = Typeface.createFromAsset(getAssets(), "font.TTF");  
+		textLocalidade.setTypeface(font);
+		textBuscar.setTypeface(font);
+		textTurismo.setTypeface(font);
+		textAjuda.setTypeface(font);
 	}
 	
 	
