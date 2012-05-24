472db075dbd40455b43f0d25005ed56b0e6bc879
juliohenriquerocha <juliohenriquerocha@a881ba28-6254-5c3f-0099-06f9c60959e8>
2012-05-24 15:51:25 +0000

    Ações visuais do menu feito.

--- Busao/src/br/edu/ufcg/dsc/busao/MenuActivity.java
+++ Busao/src/br/edu/ufcg/dsc/busao/MenuActivity.java
@@ -3,14 +3,23 @@ package br.edu.ufcg.dsc.busao;
 import it.sephiroth.demo.slider.widget.MultiDirectionSlidingDrawer;
 
 import android.app.Activity;
+import android.app.AlertDialog;
+import android.content.Context;
+import android.graphics.Color;
 import android.graphics.Typeface;
+import android.graphics.drawable.Drawable;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
 import android.widget.LinearLayout;
+import android.widget.TableRow;
 import android.widget.TextView;
 import br.edu.ufcg.dsc.R;
 
 public class MenuActivity extends Activity {
-	
+	TableRow rowLocalidade, rowBuscar, rowTurismo, rowAjuda, rowLogoBusao;
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
@@ -18,9 +27,46 @@ public class MenuActivity extends Activity {
 		setTextFont();
 		MultiDirectionSlidingDrawer drawer = (MultiDirectionSlidingDrawer) findViewById(R.id.drawer);
 		drawer.open();
+		instanciarRows();
+		setActionsRows(rowLocalidade);
+		setActionsRows(rowBuscar);
+		setActionsRows(rowTurismo);
+		setActionsRows(rowAjuda);
+		
+		//Setar aqui a ação do butão para abrir o popup
+				
+		
 		
 	}
 	
+	private void setActionsRows(final TableRow row) {
+		row.setOnClickListener(new View.OnClickListener() {
+
+	        public void onClick(View v) {
+	           limpaRows();
+	           row.setBackgroundDrawable(getResources().getDrawable(R.drawable.transparencia));
+
+	        }
+	    });
+		
+	}
+
+	private void instanciarRows() {
+		rowLocalidade = (TableRow) findViewById(R.id.rowLocalidade);
+		rowBuscar = (TableRow) findViewById(R.id.rowBuscar);
+		rowTurismo = (TableRow) findViewById(R.id.rowTurismo);
+		rowAjuda = (TableRow) findViewById(R.id.rowAjuda);
+		rowLogoBusao = (TableRow) findViewById(R.id.rowLogoBusao);
+		
+	}
+	
+	private void limpaRows(){
+		rowLocalidade.setBackgroundDrawable(null);
+		rowBuscar.setBackgroundDrawable(null);
+		rowTurismo.setBackgroundDrawable(null);
+		rowAjuda.setBackgroundDrawable(null);
+	}
+
 	/**
 	 * Limpa a pilha de Views no Linear Layout 
 	 */
